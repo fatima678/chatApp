e9338f4a51a15741223328f38a21efa139649066
fatima678 <119948111+fatima678@users.noreply.github.com>
2023-11-15 02:25:24 +0500

    designing the search bar
    designing the search bar using javascript

--- pas-show-hide.js
+++ pas-show-hide.js
@@ -4,10 +4,12 @@ const pswrdField = document.querySelector(".form input[type='password']"),
 		toggleBtn.onclick = ()=>{
 		if(pswrdField.type == "password"){
 			pswrdField.type = "text";
+			toggleBtn.classList.add("active");
 
 		}
 		else{
 			pswrdField.type = "password";
+			toggleBtn.classList.remove("active");
 
-		}
+		} 
 } 
